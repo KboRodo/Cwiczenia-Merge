6233bc307afef5e10c81fe9b18f3e002f409a47a
KboRodo <61941560+KboRodo@users.noreply.github.com>
2020-11-26 15:31:04 +0100

    123

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-///Damian Orzechowski
+/// Damian Orzechowski
 document.querySelector('#DO').style.overflowX = 'auto'
 document.querySelector('#DO').style.display = 'flex'
 document.querySelector('#DO').style.flexWrap = 'nowrap'
